cb229fb7d2d9e96f2cfc3af2d0551f1313c4376c
Aigars, Cibulskis <aigars.cibulskis@accenture.com>
2016-11-15 15:05:32 +0200

    Added required libraries

--- app/src/main/java/aiidziis/clanmanager/Settings.java
+++ app/src/main/java/aiidziis/clanmanager/Settings.java
@@ -1,8 +1,8 @@
 package aiidziis.clanmanager;
 
 /**
- * Created by aigars.cibulskis on 8/31/2016.
  * Class used to save important data for app.
+ * Created by aigars.cibulskis on 8/31/2016.
  */
 public class Settings {
     private static final String PREF_NAME = "clanManagerPref";
