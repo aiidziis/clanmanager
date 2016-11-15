e6648d8957fc6799c259d1bb3facbc7cdce9e2a3
Aigars, Cibulskis <aigars.cibulskis@accenture.com>
2016-11-15 15:43:18 +0200

    Added HomeFragment and HomePresenter

---
+++ app/src/main/java/aiidziis/clanmanager/fragment/HomeFragment.java
@@ -0,0 +1,14 @@
+package aiidziis.clanmanager.fragment;
+
+import aiidziis.clanmanager.presenter.HomePresenter;
+import nucleus.factory.RequiresPresenter;
+import nucleus.view.NucleusSupportFragment;
+
+/**
+ * HomeFragment.class for application that shows main fragment.
+ * Created by aigars.cibulskis on 11/15/2016.
+ */
+@RequiresPresenter(HomePresenter.class)
+public class HomeFragment extends NucleusSupportFragment<HomePresenter> {
+
+}
---
+++ app/src/main/java/aiidziis/clanmanager/presenter/HomePresenter.java
@@ -0,0 +1,12 @@
+package aiidziis.clanmanager.presenter;
+
+import aiidziis.clanmanager.fragment.HomeFragment;
+import nucleus.presenter.RxPresenter;
+
+/**
+ * Presenter that provides data for HomeFragment.
+ * Created by aigars.cibulskis on 11/15/2016.
+ */
+
+public class HomePresenter extends RxPresenter<HomeFragment> {
+}
