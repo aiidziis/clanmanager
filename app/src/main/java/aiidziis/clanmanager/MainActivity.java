2004c824732d8ab02862c84503f15f97447226ba
Aigars, Cibulskis <aigars.cibulskis@accenture.com>
2016-11-15 16:19:04 +0200

    Added launch of HomeFragment

--- app/src/main/java/aiidziis/clanmanager/MainActivity.java
+++ app/src/main/java/aiidziis/clanmanager/MainActivity.java
@@ -3,9 +3,16 @@ package aiidziis.clanmanager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import aiidziis.clanmanager.fragment.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, new HomeFragment())
+                .commit();
     }
 }
